e02e14ec9b3ba1ee78c692bc68b54b72a6af352e
pjlmartin@gmail.com <pjlmartin@gmail.com@17cacbb5-34f6-c1be-b6ca-193f5c6d891f>
2010-07-29 18:11:55 +0000

    added some memory info logging

--- trunk/src/googlecode/utils4ant/tasks/GC.java
+++ trunk/src/googlecode/utils4ant/tasks/GC.java
@@ -1,16 +1,24 @@
 package googlecode.utils4ant.tasks;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryPoolMXBean;
+import java.util.Iterator;
+
 import org.apache.tools.ant.Task;
 
 public class GC extends Task {
 	public void execute() {
-//		MemoryMXBean mem = ManagementFactory.getMemoryMXBean();
-//		log("before gc - heap: " + mem.getHeapMemoryUsage().getUsed()
-//				+ " bytes / permgen: " + mem.getNonHeapMemoryUsage().getUsed()
-//				+ " bytes");
+		printMemoryStatus("Before GC:");
 		System.gc();
-//		log("after  gc - heap: " + mem.getHeapMemoryUsage().getUsed()
-//				+ " bytes / permgen: " + mem.getNonHeapMemoryUsage().getUsed()
-//				+ " bytes");
+		printMemoryStatus("After GC:");
+	}
+
+	private void printMemoryStatus(String header) {
+		log(header);
+		Iterator iter = ManagementFactory.getMemoryPoolMXBeans().iterator();
+		while (iter.hasNext()) {
+			MemoryPoolMXBean item = (MemoryPoolMXBean) iter.next();
+			log("  " + item.getName() + " (" + item.getType()+ "): " + item.getUsage());
+		}
 	}
 }
